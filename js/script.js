198b2de893779a6566083affbeee5f9fdd44fe38
Lida Jiang <ljiang38@id.iit.edu>
2023-04-18 11:36:18 -0500

    add basic map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-function init() {
-  
-}
+let myMap = L.map('map').setView([37.61, -122.011], 10)
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  // Attribution is obligatory as per copyright!
+  maxZoom: 20,
+}).addTo(myMap)
+
+let sfo_marker = L.marker([37.618, -122.375]).addTo(myMap)
+let oak_marker = L.marker([37.71, -122.224]).addTo(myMap)
+let sjc_marker = L.marker([37.361, -121.928]).addTo(myMap)
